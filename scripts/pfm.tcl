5431b0a78735cad46758e66bc336064926c8c765
Rob Armstrong <robert.armstrong-jr@xilinx.com>
2019-09-05 12:05:14 -0700

    Update platform Tcl for 2019.2

--- scripts/pfm.tcl
+++ scripts/pfm.tcl
@@ -1,13 +1,10 @@
 set name zcu104_min
 
-platform create -name $name -desc "ZCU104 minimum platform" -hw [format ./dsa/%s.xsa $name] -out ./output
+platform -name $name -desc "ZCU104 minimum platform" -hw [format ./dsa/%s.xsa $name] -prebuilt -out ./output
 
-domain create -name xrt -proc psu_cortexa53_0 -os linux
-domain config -readme ./sw_platform/boot/generic.readme
-
-domain config -image ./sw_platform/image
+domain -name xrt -display-name "A53 XRT Linux" -proc psu_cortexa53 -os linux -image ./sw_platform/image
 domain config -boot ./sw_platform/boot
 domain config -bif ./generics/linux/linux.bif
-domain config -runtime opencl
+domain -runtime opencl
 
 platform -generate
